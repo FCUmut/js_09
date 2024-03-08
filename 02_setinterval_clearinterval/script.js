3af314acd1c6431cff0b0677310163bcff1a84c9
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-03-08 20:47:46 +0300

    commit
    commit

---
+++ 02_setinterval_clearinterval/script.js
@@ -0,0 +1,37 @@
+// const intervalID = setInterval(myCallback, 1000, "Hello");
+
+// function myCallback(e) {
+//   console.log(e, Date.now());
+// }
+
+let intervalID;
+
+function startChange() {
+  if (!intervalID) {
+    intervalID = setInterval(changeRandomColor, 1000);
+    // intervalID = setInterval(changeColor, 2000);
+  }
+}
+
+// function changeColor() {
+//   if (document.body.style.backgroundColor !== "black") {
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "white";
+//   } else {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+//   }
+// }
+
+function changeRandomColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+  document.body.style.backgroundColor = `#${randomColor}`;
+}
+
+function stopChange() {
+  clearInterval(intervalID);
+}
+
+document.getElementById("start").addEventListener("click", startChange);
+document.getElementById("stop").addEventListener("click", stopChange);
