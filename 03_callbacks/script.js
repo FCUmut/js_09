8a70a7f4e56a4db345866a40aba9e8496206ec52
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-03-09 22:54:23 +0300

    commit
    commit

---
+++ 03_callbacks/script.js
@@ -0,0 +1,32 @@
+// function toggle(e) {
+//   e.target.classList.toggle("danger");
+// }
+
+// document.querySelector("button").addEventListener("click", toggle);
+
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
+];
+
+function createPost(post, cb) {
+  setTimeout(() => {
+    posts.push(post);
+    cb(); // so we use callback, before createPost execute we await getPosts
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(() => {
+    posts.forEach((post) => {
+      const div = document.createElement("div");
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      document.querySelector("#posts").appendChild(div);
+    });
+  }, 1000);
+}
+
+// third post will not be shown because it takes more time to create post than getting posts to screen
+createPost({ title: "Post Three", body: "This is post three" }, getPosts);
+
+// getPosts();
